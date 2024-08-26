5f6dc7ef71e195ede7e89f52154f7a43cc1cc74b
Selim BAYINDIR <byndr28@gmail.com>
2024-08-26 14:51:31 +0300

    jsonbuilder ,mig1

--- EmployeeAdminPortal/Models/Entities/Employee.cs
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -4,8 +4,8 @@
     {
         public Guid Id { get; set; }
         public required string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public required string Email { get; set; }
+        public  string? Phone { get; set; }
         public decimal Salary { get; set; }
     }
 }
